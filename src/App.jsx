163dd1c1923c1749cffce13524c3f5bfc77f36c7
sachin <>
2023-01-04 14:01:44 +0530

    Added useeffect and display back ground image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import s from "./style.module.css";
 import { TvShowApi } from "./api/tvShow";
-
-TvShowApi.fetchPopulars();
+import { useEffect, useState } from "react";
+import { BACKDROP_BASE_URL } from "../src/config.js";
 
 export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState("");
+
+  async function fetchPopulars() {
+    const popularTVShowList = await TvShowApi.fetchPopulars();
+    if (popularTVShowList.length > 0) {
+      setCurrentTVShow(popularTVShowList[0]);
+    }
+  }
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  //console.log(currentTVShow);
+
   return (
-    <div className={s.main_container}>
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
       <div className={s.header}>
         <div className="row">
           <div className="col-4">
--- src/api/tvShow.js
+++ src/api/tvShow.js
@@ -1,16 +1,16 @@
 
 import axios from "axios";
-
-const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "?api_key=8e9fdc66cd1585ab9ee181e27839b6eb"
+import { Mock_PopularTvShow } from "./Mock_Data";
 
 export class TvShowApi{
 
     static async fetchPopulars(){
-        let response = await axios.get(`${BASE_URL}tv/popular${API_KEY}`);
+        // let response = await axios.get(`${BASE_URL}tv/popular${API_KEY}`);
 
-        console.log(response.data.results);
+        // console.log(response.data.results);
         
-        return response.data.results;
+        // return response.data.results;
+
+        return Mock_PopularTvShow();
     }
 }
